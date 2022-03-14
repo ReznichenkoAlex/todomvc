cbeb8ce1855f83fbeb37014ec34f00d32d404d71
Alexei Reznichenko <lexcooprer1@gmail.com>
2022-03-14 13:13:31 +1000

    add method render

--- src/app/Base/Controller/AbstractController.php
+++ src/app/Base/Controller/AbstractController.php
@@ -1,10 +1,24 @@
 <?php
 namespace App\Base\Controller;
 
+use App\Base\View\ViewInterface;
+
 abstract class AbstractController
 {
+	private ViewInterface $view;
+
 	public function redirect($url)
     {
         header('Location: ' . $url);
     }
+
+	public function setView(ViewInterface $view): void
+	{
+		$this->view = $view;
+	}
+
+	public function render($tpl): string
+	{
+		return $this->view->render($tpl);
+	}
 }
