f603598d95f730fbab80cb452274d51b7784e6cd
Alexei Reznichenko <lexcooprer1@gmail.com>
2022-03-16 13:47:36 +1000

    create DoctrineManager

---
+++ src/app/Base/Utils/DoctrineManager.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Base\Utils;
+
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\EntityManager;
+
+
+class DoctrineManager
+{
+	private array $dbParams = [
+		'driver'   => DB_DRIVER,
+		'user'     => DB_USER,
+		'password' => DB_PASSWORD,
+		'dbname'   => DB_NAME,
+		'host'     => DB_HOST
+	];
+
+	private               $paths = [
+		__DIR__ . "/../../Model/"
+	];
+	private EntityManager $entityManager;
+
+	public function __construct()
+	{
+		$config        = Setup::createAnnotationMetadataConfiguration(
+			$this->paths,
+			false,
+			null,
+			null,
+			false);
+		$this->entityManager = EntityManager::create($this->dbParams, $config);
+	}
+}
