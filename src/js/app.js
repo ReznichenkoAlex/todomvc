3e57e326b2d2ae2ce7011682432084742b26ab57
Alexei Reznichenko <lexcooprer1@gmail.com>
2022-03-20 19:15:12 +1000

    add render handlebars

--- src/js/app.js
+++ src/js/app.js
@@ -2,13 +2,27 @@ $(function ($) {
 	'use strict';
 
 	const App = {
-		todoItems: [],
 
 		run: async function () {
 
 			const res = await $.getJSON('/api/get');
 			this.todoItems = res.data;
+			this.todoTemplate = Handlebars.compile($('#todo-template').html());
+			this.footerTemplate = Handlebars.compile($('#footer-template').html());
 
+			$('.todo-list').html(this.todoTemplate(this.todoItems));
+			const todoCount = this.todoItems.length;
+			const activeTodoCount = this.getActiveTodos().length;
+			$('.footer').html(this.footerTemplate({
+				activeTodoCount: activeTodoCount,
+				completedTodos: todoCount - activeTodoCount
+			}));
+
+		},
+		getActiveTodos() {
+			return this.todoItems.filter(todo => {
+				return !todo.isCompleted;
+			});
 		}
 	}
 
