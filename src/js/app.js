9ea1ea544e04927bc18dffc2f24f489ab0c5c314
Alexei Reznichenko <lexcooprer1@gmail.com>
2022-03-21 17:52:55 +1000

    refactor code

--- src/js/app.js
+++ src/js/app.js
@@ -8,6 +8,15 @@ $(function ($) {
 	});
 
 	const App = {
+		run: async function () {
+			this.todoItems = await this.getTodos();
+			this.todoTemplate = Handlebars.compile($('#todo-template').html());
+			this.footerTemplate = Handlebars.compile($('#footer-template').html());
+			this.filter = 'all';
+			this.render();
+			this.initEvents();
+		},
+
 		render: function () {
 			let todos = this.getFilteredTodos();
 
@@ -27,6 +36,21 @@ $(function ($) {
 			$('.new-todo').focus();
 		},
 
+		initEvents: function () {
+			$('.new-todo').on('keyup', this.createTask.bind(this));
+
+			$('.todo-list')
+				.on('click', '.toggle', this.toggleTask.bind(this))
+				.on('click', '.destroy', this.deleteTask.bind(this))
+				.on('dblclick', 'label', this.switchEditingMode.bind(this))
+				.on('keyup', '.edit', this.emitFocusout.bind(this))
+				.on('focusout', '.edit', this.updateTask.bind(this));
+
+			$('.toggle-all').on('click', this.toggleAll.bind(this));
+			$('.clear-completed').on('click', this.deleteCompletedTasks.bind(this));
+			$('.filters').on('click', 'li', this.filterTasks.bind(this))
+		},
+
 		getFilteredTodos: function () {
 			if (this.filter === 'active') {
 				return this.getActiveTodos();
@@ -38,6 +62,63 @@ $(function ($) {
 			return this.todoItems;
 		},
 
+		getCompletedTasks: function () {
+			return this.todoItems.filter(todo => {
+				return todo.isCompleted;
+			});
+		},
+
+		getActiveTodos: function () {
+			return this.todoItems.filter(todo => {
+				return !todo.isCompleted;
+			});
+		},
+
+		getTodos: async function () {
+			let data = JSON.parse(localStorage.getItem('data'));
+			if (!data || data.length === 0) {
+				const res = await $.getJSON('/api/get');
+				data = res.data;
+				localStorage.setItem('data', JSON.stringify(data));
+			}
+			return data;
+		},
+
+		createTask: function (e) {
+			let $input = $(e.target)
+			let value = $input.val().trim();
+			if (e.which === 13 && value) {
+				const task = {
+					uuid: this.uuidv4(),
+					title: value,
+					isCompleted: false
+				}
+				this.todoItems.push(task);
+				$input.val('');
+				this.render();
+				this.sendAjaxJson('api/post', task, 'POST');
+			}
+		},
+
+		updateTask: function (e) {
+			let el = e.target;
+			let $el = $(el);
+			let val = $el.val().trim();
+
+			if ($el.data('abort')) {
+				$el.data('abort', false);
+			} else if (!val) {
+				this.deleteTask(e);
+				return;
+			} else {
+				const index = this.getIndex(e)
+				this.todoItems[index].title = val;
+				this.sendAjaxJson('api/patch', this.todoItems[index], 'PATCH');
+			}
+
+			this.render();
+		},
+
 		deleteTask: function (e) {
 			const index = this.getIndex(e);
 			this.sendAjaxJson('api/delete', {uuid: this.todoItems[index].uuid}, 'DELETE');
@@ -45,61 +126,29 @@ $(function ($) {
 			this.render();
 		},
 
-		run: async function () {
-			this.todoItems = await this.getTodos();
-			this.todoTemplate = Handlebars.compile($('#todo-template').html());
-			this.footerTemplate = Handlebars.compile($('#footer-template').html());
-			this.filter = 'all';
+		toggleTask: function (e) {
+			const index = this.getIndex(e);
+			this.todoItems[index].isCompleted = !this.todoItems[index].isCompleted;
+			this.sendAjaxJson('api/patch', this.todoItems[index], 'PATCH');
 			this.render();
-			this.initEvents();
 		},
 
-		initEvents: function () {
-			$('.new-todo').on('keyup', this.createTask.bind(this));
-
-			$('.todo-list')
-				.on('click', '.toggle', this.togleTask.bind(this))
-				.on('click', '.destroy', this.deleteTask.bind(this))
-				.on('dblclick', 'label', this.switchEditingMode.bind(this))
-				.on('keyup', '.edit', function (e) {
-					if (e.which === 13) {
-						e.target.blur();
-					}
-					if (e.which === 27) {
-						$(e.target).data('abort', true).blur();
-					}
-				}.bind(this))
-				.on('focusout', '.edit', function (e) {
-					let el = e.target;
-					let $el = $(el);
-					let val = $el.val().trim();
-
-					if ($el.data('abort')) {
-						$el.data('abort', false);
-					} else if (!val) {
-						this.deleteTask(e);
-						return;
-					} else {
-						const index = this.getIndex(e)
-						this.todoItems[index].title = val;
-						this.sendAjaxJson('api/patch', this.todoItems[index], 'PATCH');
-					}
-
-					this.render();
-			}.bind(this));
-
-			$('.toggle-all').on('click', this.toggleAll.bind(this));
-			$('.clear-completed').on('click', this.deleteCompletedTasks.bind(this));
-			$('.filters').on('click', 'li', this.filterTasks.bind(this))
+		deleteCompletedTasks: function () {
+			this.getCompletedTasks().forEach(todo => {
+				this.sendAjaxJson('api/delete', {uuid: todo.uuid}, 'DELETE');
+			});
+			this.todoItems = this.getActiveTodos();
+			this.render();
 		},
 
-		switchEditingMode: function (e) {
-			let $input = $(e.target).closest('li').addClass('editing').find('.edit');
+		toggleAll: function (e) {
+			let toggleBool = $(e.target).prop('checked');
 
-			let tmpStr = $input.val();
-			$input.val('');
-			$input.val(tmpStr);
-			$input.focus();
+			this.todoItems.forEach(todo => {
+				todo.isCompleted = toggleBool;
+				this.sendAjaxJson('api/patch', todo, 'PATCH');
+			})
+			this.render();
 		},
 
 		filterTasks: function (e) {
@@ -119,48 +168,6 @@ $(function ($) {
 			this.render();
 		},
 
-		createTask: function (e) {
-			let $input = $(e.target)
-			let value = $input.val().trim();
-			if (e.which === 13 && value) {
-				const task = {
-					uuid: this.uuidv4(),
-					title: value,
-					isCompleted: false
-				}
-				this.todoItems.push(task);
-				$input.val('');
-				this.render();
-				this.sendAjaxJson('api/post', task, 'POST');
-			}
-		},
-
-		uuidv4: function () {
-			return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
-				(c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
-			);
-		},
-
-		deleteCompletedTasks: function () {
-			this.getCompletedTasks().forEach(todo => {
-				this.sendAjaxJson('api/delete', {uuid: todo.uuid}, 'DELETE');
-			});
-			this.todoItems = this.getActiveTodos();
-			this.render();
-		}
-		,
-
-		toggleAll: function (e) {
-			let toggleBool = $(e.target).prop('checked');
-
-			this.todoItems.forEach(todo => {
-				todo.isCompleted = toggleBool;
-				this.sendAjaxJson('api/patch', todo, 'PATCH');
-			})
-			this.render();
-		}
-		,
-
 		sendAjaxJson: function (url, data, method = 'GET') {
 			$.ajax({
 					url: url,
@@ -170,40 +177,17 @@ $(function ($) {
 					processData: false,
 				}
 			)
-		}
-		,
+		},
 
-		togleTask: function (e) {
-			const index = this.getIndex(e);
-			this.todoItems[index].isCompleted = !this.todoItems[index].isCompleted;
-			this.sendAjaxJson('api/patch', this.todoItems[index], 'PATCH');
-			this.render();
-		}
-		,
-
-		getCompletedTasks: function () {
-			return this.todoItems.filter(todo => {
-				return todo.isCompleted;
-			});
-		}
-		,
-
-		getActiveTodos: function () {
-			return this.todoItems.filter(todo => {
-				return !todo.isCompleted;
-			});
-		}
-		,
-		getTodos: async function () {
-			let data = JSON.parse(localStorage.getItem('data'));
-			if (!data || data.length === 0) {
-				const res = await $.getJSON('/api/get');
-				data = res.data;
-				localStorage.setItem('data', JSON.stringify(data));
+		emitFocusout: function (e) {
+			if (e.which === 13) {
+				e.target.blur();
 			}
-			return data;
-		}
-		,
+			if (e.which === 27) {
+				$(e.target).data('abort', true).blur();
+			}
+		},
+
 		getIndex: function (e) {
 			const uuid = $(e.target).closest('li').data().id;
 			let todos = this.todoItems
@@ -214,6 +198,21 @@ $(function ($) {
 					return i;
 				}
 			}
+		},
+
+		switchEditingMode: function (e) {
+			let $input = $(e.target).closest('li').addClass('editing').find('.edit');
+
+			let tmpStr = $input.val();
+			$input.val('');
+			$input.val(tmpStr);
+			$input.focus();
+		},
+
+		uuidv4: function () {
+			return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
+				(c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+			);
 		}
 	}
 
